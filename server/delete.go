dc6d5469dd5344a9bcbf10a02f5362b22a2e3633
ebishrimp <ebishrimp6022@gmail.com>
2026-04-22 18:14:59 +0900

    getting filepath

--- server/delete.go
+++ server/delete.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func DeleteOperation(db *sql.DB, name string, hard string, app string, w http.ResponseWriter) {
@@ -12,8 +13,26 @@ func DeleteOperation(db *sql.DB, name string, hard string, app string, w http.Re
 		http.Error(w, "Error selecting file information", http.StatusInternalServerError)
 		return
 	}
+	selectPath, err := db.Query("SELECT path FROM filepath WHERE filename = ? AND hardlayer = ? AND applayer = ?", name, hard, app)
+	if err != nil {
+		http.Error(w, "Error selecting file path", http.StatusInternalServerError)
+		return
+	}
 	defer selectID.Close()
+	defer selectPath.Close()
 
+	//path operation
+	if selectPath.Next() {
+		var path string
+		err := selectPath.Scan(&path)
+		if err != nil {
+			http.Error(w, "Error scanning path", http.StatusInternalServerError)
+			return
+		}
+		deleteDirOp(path, w)
+	}
+
+	//id operation
 	if selectID.Next() {
 		var id int
 		err := selectID.Scan(&id)
@@ -30,4 +49,8 @@ func DeleteOperation(db *sql.DB, name string, hard string, app string, w http.Re
 	} else {
 		http.Error(w, "No file information found for the given parameters", http.StatusNotFound)
 	}
+}
+
+func deleteDirOp(path string, w http.ResponseWriter) {
+	err := os.Remove(path)
 }
