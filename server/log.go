515c19e501aee643d8b00452dd036e2b9bec0101
ebishrimp <ebishrimp6022@gmail.com>
2026-05-26 09:12:57 +0900

    access logging to /var/log/fileserver.log

--- server/log.go
+++ server/log.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"compress/gzip"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
+	"time"
 )
 
 type AccessLog struct {
@@ -13,10 +17,11 @@ type AccessLog struct {
 	Error     error
 }
 
-func (log *AccessLog) WriteLog(path string) {
+func (logstat *AccessLog) WriteLog(path string) {
 	var compressPath string
+	var log *os.File
 
-	if fileSizeLarge(logfile) {
+	if fileSizeLarge(path) {
 		idx := 1
 		exists := true
 		for exists {
@@ -28,7 +33,39 @@ func (log *AccessLog) WriteLog(path string) {
 			}
 		}
 		//write log to compressPath
-		compressLog(compressPath)
+		err := compressLog(compressPath, path)
+		if err != nil {
+			fmt.Printf("failed to compress logfile: %s\n, err: %s", compressPath, err)
+		}
+		f, err := os.Create(path)
+		if err != nil {
+			fmt.Printf("failed to open the logfile. path: %s\n", path)
+		}
+		log = f
+	} else {
+		f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Printf("failed to open the logfile. path: %s\n", path)
+		}
+		log = f
+	}
+	defer log.Close()
+
+	writer := bufio.NewWriter(log)
+	currentTime := time.Now().Format("2006-01-02T15:05:04")
+
+	var errcontent string
+	if logstat.Error == nil {
+		errcontent = "No error"
+	} else {
+		errcontent = logstat.Error.Error()
+	}
+	logContent := fmt.Sprintf("[%s] Client: %s, Operation: %s, Path: %s, Error: %s\n", currentTime, logstat.IP, logstat.Operation, logstat.Path, errcontent)
+
+	_, err := writer.WriteString(logContent)
+	if err != nil {
+		fmt.Println("failed to write log: %w", err)
+		return
 	}
 
 }
@@ -42,6 +79,28 @@ func fileSizeLarge(path string) bool {
 	return logfileinfo.Size() > int64(maxlogfilesize)
 }
 
-func compressLog(path string) error {
+func compressLog(path string, logsrc string) error {
+	dist, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer dist.Close()
+
+	gw, err := gzip.NewWriterLevel(dist, gzip.BestCompression)
+	if err != nil {
+		return err
+	}
+	defer gw.Close()
+
+	src, err := os.Open(logsrc)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	if _, err := io.Copy(gw, src); err != nil {
+		return err
+	}
+
 	return nil
 }
